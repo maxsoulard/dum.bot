e4de2e5335546d8cbc4cd2ec58855059a29f87dc
maxime.soulard <maxime.soulard.ext@labanquepostale-assurances-iard.fr>
2015-02-20 10:57:37 +0100

    html

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ class Utils:
 
     @staticmethod
     def angletodutycycle(angle=None):
+        # y = ax+b où a=5/90 et b=2.5 (pour SG90, 50Hz)
         dutycycle = None
         if angle is not None:
             dutycycle = (5.0*angle)/90 + 2.5
@@ -10,6 +11,7 @@ class Utils:
 
     @staticmethod
     def dutycycletoangle(dutycycle=None):
+        # x = (y-b)/a où a=5/90 et b=2.5 (pour SG90, 50Hz)
         angle = None
         if dutycycle is not None:
             angle = (dutycycle - 2.5) * (90/5)
