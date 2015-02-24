21e6e0287e7162a13721f8ea467f9859ad3d6422
maxime.soulard <maxime.soulard.ext@labanquepostale-assurances-iard.fr>
2015-02-24 09:39:51 +0100

    Utils

--- utils.py
+++ utils.py
@@ -16,3 +16,64 @@ class Utils:
         if dutycycle is not None:
             angle = (dutycycle - 2.5) * (90/5)
         return angle
+
+	# Conversions dec - bin
+	@staticmethod
+    def convertdecbin(param=0):
+        if not isinstance(param, int):
+            return None
+        resultdivision = param
+        result = []
+        while resultdivision >= 1:
+            if param != -1:
+                reste = resultdivision % 2
+                result.append(str(reste))
+                resultdivision /= 2
+        result.append(str(resultdivision))
+        result.reverse()
+        return ''.join(result)
+
+    @staticmethod
+    def convertbindec(param=''):
+        decimal = 0
+        index = len(param)-1
+        for c in param:
+            if c == '1':
+                result = math.pow(2, index)
+                decimal += result
+            elif c != '0':
+                decimal = None
+                break
+            index -= 1
+
+        return decimal
+
+    @staticmethod
+    def manchesterdecode(param=''):
+        result = []
+        cc = []
+        for c in param:
+            cc.append(c)
+            if len(cc) == 2:
+                if cc[0] == '0' and cc[1] == '1':
+                    result.append('0')
+                elif cc[0] == '1' and cc[1] == '0':
+                    result.append('1')
+                else:
+                    result = None
+                    break
+                cc = []
+        return ''.join(result)
+
+    @staticmethod
+    def manchesterencode(param=''):
+        result = []
+        for c in param:
+            if c == '0':
+                result.append("01")
+            elif c == '1':
+                result.append("10")
+            else:
+                result = None
+                break
+        return ''.join(result)
