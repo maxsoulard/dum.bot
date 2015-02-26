bd4fe8d03b5f2c8df701f032c77fc0771b1533fc
maxetx <max.soulard@gmail.com>
2015-02-26 14:09:11 +0100

    CSS

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 cd /usr/src/mjpg-streamer/mjpg-streamer-experimental
 export LD_LIBRARY_PATH=.
-./mjpg_streamer -o "output_http.so -w ./www -p 8083" -i "input_raspicam.so -x 640 -y 480 -fps 15 -ex night" &
+./mjpg_streamer -o "output_http.so -w ./www -p 8083" -i "input_raspicam.so -x 1280 -y 1024 -fps 20 -ex night" &
 
 cd /home/pi/workspace/dum.bot
 sudo python webdumbot.py &
