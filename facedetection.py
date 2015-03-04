f9f0c4658f4892d718dbb258e87d911c78560a06
maxetx <max.soulard@gmail.com>
2015-03-04 22:21:32 +0100

    dev facedetection

--- facedetection.py
+++ facedetection.py
@@ -1,20 +1,23 @@
 import numpy as np
 import cv2
+from threading import Thread
 
-face_cascade = cv2.CascadeClassifier('lbpcascade_frontalface.xml')
-#eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-print "face_cascade OK"
+class Facedetection(Thread):
 
-#TODO
-#jc = JpegStreamCamera("http://192.168.1.15:8083/?action=stream")
-#img = jc.getImage().save("tmp.jpg")
-#resize img -50% to 320x240
+    def run(self):
+        face_cascade = cv2.CascadeClassifier('lbpcascade_frontalface.xml')
 
-img = cv2.imread('test3.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        print "face_cascade OK"
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-#eyes = eye_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-        print str(x)+" "+str(y)+" "+str(w)+" "+str(h)
+        #TODO
+        jc = JpegStreamCamera("http://localhost:8083/?action=stream")
+        img = jc.getImage()
+        #resize img -50% to 320x240
+
+        # img = cv2.imread('tmp.jpg')
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+                print str(x)+" "+str(y)+" "+str(w)+" "+str(h)
