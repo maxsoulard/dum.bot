882ddba48daef7c9639126fb7f6ba29797ee3fb0
maxetx <max.soulard@gmail.com>
2015-03-07 17:37:51 +0100

    It's alive

--- gpiodcmotors.py
+++ gpiodcmotors.py
@@ -12,30 +12,38 @@ class Gpiodcmotors:
         GPIO.setup(7, GPIO.OUT)
         GPIO.setup(8, GPIO.OUT)
         GPIO.setup(10, GPIO.OUT)
-        GPIO.setup(12, GPIO.OUT)
+        GPIO.setup(16, GPIO.OUT)
         self._reinitLedBool()
 
     def triggerForward(self):
         self.led10 = not self.led10
         GPIO.output(10, self.led10)
+	self.led7 = not self.led7
+	GPIO.output(7, self.led7)
 
     def triggerBackward(self):
         self.led12 = not self.led12
-        GPIO.output(12, self.led12)
-
-    def triggerLeft(self):
-        self.led8 = not self.led8
-        GPIO.output(8, self.led8)
+        GPIO.output(16, self.led12)
+	self.led8 = not self.led8
+	GPIO.output(8, self.led8)
 
     def triggerRight(self):
+        self.led8 = not self.led8
+        GPIO.output(8, self.led8)
+	self.led10 = not self.led10
+	GPIO.output(10, self.led10)
+
+    def triggerLeft(self):
         self.led7 = not self.led7
         GPIO.output(7, self.led7)
+	self.led12 = not self.led12
+	GPIO.output(16, self.led12)
 
     def reset(self):
         self._reinitLedBool()
         GPIO.output(7, False)
         GPIO.output(10, False)
-        GPIO.output(12, False)
+        GPIO.output(16, False)
         GPIO.output(8, False)
 
     def stop(self):
